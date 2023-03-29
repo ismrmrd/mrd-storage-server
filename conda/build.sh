d55fa5c44b569b07e6938aae1010b63ac5a38c4f
John Stairs <jostairs@microsoft.com>
2023-03-29 18:42:36 +0000

    Cleanup

--- conda/build.sh
+++ conda/build.sh
@@ -2,11 +2,4 @@
 
 set -euo pipefail
 
-echo "build.sh: env..."
-env
-
-echo "build.sh: which go..."
-which go
-
-echo "build.sh: go build..."
-go build -v -o "${PREFIX}/bin/mrd-storage-server" .
+go build -o "${PREFIX}/bin/mrd-storage-server" .
